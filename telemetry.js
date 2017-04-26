a2d4a01eeb6557f0bf3e203c38821665e0e4b58e
Yev Krupetsky <yevk@babator.com>
2017-04-26 18:17:03 +0300

    Initial

---
+++ telemetry.js
@@ -0,0 +1,61 @@
+;(function () {
+	function onBabatorReady(callback) {
+		if (Babator && Babator.isReady) {
+			callback();
+		} else {
+			window.addEventListener('babator-ready', function babatorReadyListener() {
+				window.removeEventListener('babator-ready', babatorReadyListener);
+				callback();
+			});
+		}
+	}
+
+	onBabatorReady(function () {
+
+		Babator.telemetry.on('videoPlayerLoad', function (_, data) {
+			console.log('-----------| PLAYER LOAD |', data);
+		});
+
+		Babator.telemetry.on('videoAdStart', function (_, data) {
+			console.log('-----------| AD START |', data);
+		});
+
+		Babator.telemetry.on('videoStart', function (_, data) {
+			console.log('-----------| VIDEO START |', data);
+		});
+
+		Babator.telemetry.on('videoContentPlay', function (_, data) {
+			console.log('-----------| VIDEO CONTENT PLAY |', data);
+		});
+
+		Babator.telemetry.on('videoPause', function (_, data) {
+			console.log('-----------| VIDEO PAUSE |', data);
+		});
+
+		Babator.telemetry.on('videoComplete', function (_, data) {
+			console.log('-----------| VIDEO COMPLETE |', data);
+		});
+
+		Babator.telemetry.on('videoAdComplete', function (_, data) {
+			console.log('-----------| VIDEO AD COMPLETE |', data);
+		});
+
+		Babator.telemetry.on('videoViewCheckpoint', function (_, data) {
+			console.log('-----------| VIDEO QUARTER 1 |', data);
+		});
+
+		Babator.telemetry.on('videoView25Checkpoint', function (_, data) {
+			console.log('-----------| VIDEO QUARTER 2 |', data);
+		});
+
+		Babator.telemetry.on('videoView50Checkpoint', function (_, data) {
+			console.log('-----------| VIDEO QUARTER 3 |', data);
+		});
+
+		Babator.telemetry.on('videoView75Checkpoint', function (_, data) {
+			console.log('-----------| VIDEO QUARTER 4 |', data);
+		});
+
+	});
+
+})();
