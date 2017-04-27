3db01f58262bc35443889e9fa018fe36f00a9295
Boaz Avidan <boaz.avidan@babator.com>
2017-04-27 13:50:25 +0300

    Change order and add videoRequest

--- telemetry.js
+++ telemetry.js
@@ -12,17 +12,25 @@
 
 	onBabatorReady(function () {
 
+        Babator.telemetry.on('videoRequest', function (_, data) {
+            console.log('-----------| VIDEO REQUEST |', data);
+        });
+
 		Babator.telemetry.on('videoPlayerLoad', function (_, data) {
 			console.log('-----------| PLAYER LOAD |', data);
 		});
+
+        Babator.telemetry.on('videoStart', function (_, data) {
+            console.log('-----------| VIDEO START |', data);
+        });
 
 		Babator.telemetry.on('videoAdStart', function (_, data) {
 			console.log('-----------| AD START |', data);
 		});
 
-		Babator.telemetry.on('videoStart', function (_, data) {
-			console.log('-----------| VIDEO START |', data);
-		});
+        Babator.telemetry.on('videoAdComplete', function (_, data) {
+            console.log('-----------| VIDEO AD COMPLETE |', data);
+        });
 
 		Babator.telemetry.on('videoContentPlay', function (_, data) {
 			console.log('-----------| VIDEO CONTENT PLAY |', data);
@@ -30,18 +38,6 @@
 
 		Babator.telemetry.on('videoPause', function (_, data) {
 			console.log('-----------| VIDEO PAUSE |', data);
-		});
-
-		Babator.telemetry.on('videoComplete', function (_, data) {
-			console.log('-----------| VIDEO COMPLETE |', data);
-		});
-
-		Babator.telemetry.on('videoAdComplete', function (_, data) {
-			console.log('-----------| VIDEO AD COMPLETE |', data);
-		});
-
-		Babator.telemetry.on('videoViewCheckpoint', function (_, data) {
-			console.log('-----------| VIDEO QUARTER 1 |', data);
 		});
 
 		Babator.telemetry.on('videoView25Checkpoint', function (_, data) {
@@ -55,6 +51,10 @@
 		Babator.telemetry.on('videoView75Checkpoint', function (_, data) {
 			console.log('-----------| VIDEO QUARTER 4 |', data);
 		});
+
+        Babator.telemetry.on('videoComplete', function (_, data) {
+            console.log('-----------| VIDEO COMPLETE |', data);
+        });
 
 	});
 
